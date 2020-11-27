3e406ba1cb20f739db8c041127c0d8b6316f4b3b
Victor Badenas <victor.badenas@gmail.com>
2020-11-27 13:10:30 +0100

    added logfile

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import argparse
+import logging
 import math
 import pandas as pd
-from DataRetriever import DataRetriever
+from pathlib import Path
+from dataRetriever import DataRetriever
 import matplotlib.pyplot as plt
+from utils import set_logger, show_parameters
 
 def main(args):
     for dataId in args.ids:
@@ -23,8 +26,12 @@ def ParseArgumentsFromCommandLine():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--ids', action="append", default=["623z-r97q", "jj6z-iyrp"])
     parser.add_argument('-s', '--show', action="store_true", default=False)
+    parser.add_argument("-d", "--debug", action="store_true", default=False)
+    parser.add_argument("--log_file", type=Path, default="./log/train.log")
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = ParseArgumentsFromCommandLine()
+    set_logger(args.log_file, args.debug)
+    show_parameters(args)
     main(args)
---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import sys
+import logging
+from pathlib import Path
+
+def set_logger(log_file_path, debug=False):
+    level = logging.DEBUG if debug else logging.INFO
+    logging_format = '[%(asctime)s][%(filename)s(%(lineno)d):%(funcName)s]-%(levelname)s: %(message)s'
+    log_file_path.parent.mkdir(parents=True, exist_ok=True)
+    logging.basicConfig(filename=log_file_path, level=level, format=logging_format)
+    consoleHandler = logging.StreamHandler(sys.stdout)
+    consoleHandler.setFormatter(logging.Formatter(logging_format))
+    logging.getLogger().addHandler(consoleHandler)
+
+def show_parameters(parameters):
+    logging.info("Training with parameters:")
+    for label, value in parameters.__dict__.items():
+        logging.info(f"{label}: {value}")
