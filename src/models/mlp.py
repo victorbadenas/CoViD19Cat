f04b2b6132cf4d149241fe40df1e32650cc3a8c0
Victor Badenas <victor.badenas@gmail.com>
2020-12-29 21:11:24 +0100

    changed to cvEvaluateModel

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -1,12 +1,14 @@
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import mean_squared_error
 from itertools import product
+from utils import cvEvaluateModel
 import logging
+import numpy as np
 
-def findBestMlp(X, Y, xTrain, xTest, yTrain, yTest):
+def findBestMlp(X, Y):
 
-    hiddenSizes = list(product([10, 25, 100], [10, 25, 100]))
+    hiddenSizes = list(product([5, 8, 10], [5, 8, 10]))
+    hiddenSizes += [(5,), (8,), (10,)]
     learning_rate = ['constant', 'invscaling', 'adaptive']
     params = dict(hidden_layer_sizes=hiddenSizes,
                   learning_rate=learning_rate)
@@ -21,7 +23,13 @@ def findBestMlp(X, Y, xTrain, xTest, yTrain, yTest):
     mlpcv = mlpcv.fit(X, Y)
     best_params = mlpcv.best_params_
 
-    finalMlp = MLPRegressor(max_iter=1000, tol=1e-6, batch_size=10, **best_params).fit(xTrain, yTrain)
+    finalMlp = MLPRegressor(max_iter=1000, tol=1e-6, batch_size=10, **best_params)
+
+    metrics, finalMlp = cvEvaluateModel(X, Y, finalMlp)
+
     allprediction = finalMlp.predict(X)
+
     logging.info(f"best params for MLP: {best_params}")
-    return allprediction[:, 0], allprediction[:, 1], allprediction[:, 2], best_params
+    logging.info(f"best model with metrics: {metrics}")
+
+    return allprediction[:, 0], allprediction[:, 1], allprediction[:, 2], best_params, metrics
