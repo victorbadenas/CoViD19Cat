e0be1120569e45b3a447c129cdb6abd3328dac14
Victor Badenas <victor.badenas@gmail.com>
2020-12-29 21:10:12 +0100

    removed split and added model evaluation funcion

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 import sys
 import logging
 from pathlib import Path
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error, r2_score, max_error
+import numpy as np
 
 def set_logger(log_file_path, debug=False):
     level = logging.DEBUG if debug else logging.INFO
@@ -15,3 +18,34 @@ def show_parameters(parameters):
     logging.info("Called with parameters:")
     for label, value in parameters.__dict__.items():
         logging.info(f"\t{label}: {value}")
+
+def cvEvaluateModel(X, Y, model, return_last_fitted=True):
+    assert hasattr(model, 'fit'), 'model does not have a fit method'
+    assert hasattr(model, 'predict'), 'model does not have a predict method'
+
+    mse = []
+    r2 = []
+    max_e = []
+
+    kf = KFold(10, random_state=0, shuffle=True)
+    for train_index, test_index in kf.split(X): 
+        xTrain, yTrain = X[train_index], Y[train_index]
+        xTest, yTest = X[test_index], Y[test_index]
+
+        model.fit(xTrain, yTrain)
+        foldPrediction = model.predict(xTest)
+        
+        foldMse = list(map(lambda i: mean_squared_error(foldPrediction[:, i], yTest[:, i]), range(yTest.shape[1])))
+        foldR2 = list(map(lambda i: r2_score(foldPrediction[:, i], yTest[:, i]), range(yTest.shape[1])))
+        foldMaxError = list(map(lambda i: max_error(foldPrediction[:, i], yTest[:, i]), range(yTest.shape[1])))
+
+        mse.append(foldMse)
+        r2.append(foldR2)
+        max_e.append(foldMaxError)
+
+    mse = np.average(np.array(mse), axis=0)
+    r2 = np.average(np.array(r2), axis=0)
+    max_e = np.average(np.array(max_e), axis=0)
+
+    metrics = {'mse':list(mse), 'r2':list(r2), 'max_e':list(max_e)}
+    return metrics, model if return_last_fitted else metrics
