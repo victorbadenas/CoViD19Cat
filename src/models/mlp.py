a5c7a97dc8e15c92dfab8bb920c9662a87d55795
Victor <victor.badenas@gmail.com>
2020-12-29 20:06:47 +0100

    infer all data added on best mlp

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -1,19 +1,17 @@
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error
+from itertools import product
+import logging
 
-def findBestMlp(X, Y, xTrain, yTrain, xTest, yTest):
+def findBestMlp(X, Y, xTrain, xTest, yTrain, yTest):
 
-    hiddenSizes = [(10,), (50,), (100,), (100, 10), (100, 50)]
-    activation = ['identity', 'logistic', 'tanh', 'relu']
-    batch_size = [1, 5, 10, 50]
+    hiddenSizes = list(product([10, 25, 100], [10, 25, 100]))
     learning_rate = ['constant', 'invscaling', 'adaptive']
     params = dict(hidden_layer_sizes=hiddenSizes,
-                  activation=activation,
-                  batch_size=batch_size,
                   learning_rate=learning_rate)
 
-    mlpcv = GridSearchCV(MLPRegressor(max_iter=1000, tol=1e-6), 
+    mlpcv = GridSearchCV(MLPRegressor(max_iter=1000, tol=1e-6, batch_size=10), 
                         params,
                         scoring='neg_mean_squared_error',
                         n_jobs=-1,
@@ -21,4 +19,9 @@ def findBestMlp(X, Y, xTrain, yTrain, xTest, yTest):
                         refit=False,
                         verbose=2)
     mlpcv = mlpcv.fit(X, Y)
-    return mlpcv.best_params_
+    best_params = mlpcv.best_params_
+
+    finalMlp = MLPRegressor(max_iter=1000, tol=1e-6, batch_size=10, **best_params).fit(xTrain, yTrain)
+    allprediction = finalMlp.predict(X)
+    logging.info(f"best params for MLP: {best_params}")
+    return allprediction[:, 0], allprediction[:, 1], allprediction[:, 2], best_params
