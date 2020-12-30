de3d72c27d3b7ba9703a84ebf43a658111c83aa5
Victor Badenas <victor.badenas@gmail.com>
2020-12-30 11:27:17 +0100

    fixed dataset not formatting when downloaded

--- src/dataPreprocessor.py
+++ src/dataPreprocessor.py
@@ -14,8 +14,10 @@ def createPivotTable(data, newdf, id, tag):
     databyRegion = data[columns].dropna()
 
     if id == 0 or id == 1:
+        databyRegion['numcasos'] = databyRegion['numcasos'].apply(int)
         databyRegion = databyRegion.pivot_table(index=['date'], columns='comarcadescripcio', aggfunc=sum).fillna(0.0)
     if id > 1:
+        databyRegion['numexitus'] = databyRegion['numexitus'].apply(int)
         databyRegion = databyRegion.pivot_table(index=['date'], columns='sexedescripcio',aggfunc=sum).fillna(0.0)
     if id == 0 or id == 2:
         databyRegion = databyRegion['numcasos']
