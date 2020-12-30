3576bbc8b4e2712628a4195d16f243ead5cd8bd6
Victor Badenas <victor.badenas@gmail.com>
2020-12-30 22:35:02 +0100

    added normalizeData

--- src/main.py
+++ src/main.py
@@ -3,10 +3,13 @@ import logging
 from pathlib import Path
 from dataRetriever import DataRetriever
 from utils import set_logger, show_parameters
-from dataPreprocessor import preprocessData, normalizeData
+from dataPreprocessor import preprocessData, customNormalizer
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from models import findBestSVR, findBestRF, findBestMlp, findBestAda
+from augmentData import augmentData
+
+DATA_AUGMENT_RATIO = 3
 
 def main(args):
 
@@ -14,46 +17,59 @@ def main(args):
     data_pos = DataRetriever(args.ids[0])()
     data_death = DataRetriever(args.ids[1])()
     dataset = preprocessData(data_pos, data_death)
-    data, dates = normalizeData(dataset)
+    normalizer = customNormalizer()
+    data, dates = normalizer.normalizeData(dataset)
 
     X = data[:-1]
     Y = data[:,[0,1,-1]][1:]
+
+    X, Y = augmentData(X, Y)
 
     infectedSvrPred, deathsSvrPred, r0SvrPred, svrMetrics = findBestSVR(X, Y)
     infectedAdaPred, deathsAdaPred, r0AdaPred, adaMetrics = findBestAda(X, Y)
     infectedRfPred, deathsRfPred, r0RfPred, bestRfParams, rfMetrics = findBestRF(X, Y)
     infectedMlpPred, deathsMlpPred, r0MlpPred, bestMlpParams, mlpMetrics = findBestMlp(X, Y)
 
+    final_sample = X.shape[0]//DATA_AUGMENT_RATIO
+
+    # infectedSvrPred, deathsSvrPred, r0SvrPred = normalizer.inverse_transform(infectedSvrPred, deathsSvrPred, r0SvrPred)
+    # infectedAdaPred, deathsAdaPred, r0AdaPred = normalizer.inverse_transform(infectedAdaPred, deathsAdaPred, r0AdaPred)
+    # infectedRfPred, deathsRfPred, r0RfPred = normalizer.inverse_transform(infectedRfPred, deathsRfPred, r0RfPred)
+    # infectedMlpPred, deathsMlpPred, r0MlpPred = normalizer.inverse_transform(infectedSvrPred, deathsSvrPred, r0SvrPred)
+    # positiveTruth, deathsTruth, r0Truth = normalizer.inverse_transform(Y[:, 0], Y[:, 1], Y[:, 2])
+    positiveTruth, deathsTruth, r0Truth = Y[:, 0], Y[:, 1], Y[:, 2]
+
     f, ax = plt.subplots(3, 1, sharex=True, figsize=(10, 7))
 
-    ax[0].plot(infectedRfPred, c='r', label='rf_predicted')
-    ax[0].plot(infectedMlpPred, c='g', label='mlp_predicted')
-    ax[0].plot(infectedAdaPred, c='k', label='ada_predicted')
-    ax[0].plot(infectedSvrPred, c='m', label='svr_predicted')
-    ax[0].plot(Y[:, 0], c='b', label='truth')
+    ax[0].plot(infectedRfPred[:final_sample], c='r', label='rf_predicted')
+    ax[0].plot(infectedMlpPred[:final_sample], c='g', label='mlp_predicted')
+    ax[0].plot(infectedAdaPred[:final_sample], c='k', label='ada_predicted')
+    ax[0].plot(infectedSvrPred[:final_sample], c='m', label='svr_predicted')
+    ax[0].plot(positiveTruth[:final_sample], c='b', label='truth')
     ax[0].set_ylabel('num_infected')
     ax[0].grid('on')
     ax[0].legend()
 
-    ax[1].plot(deathsRfPred, c='r', label='rf_predicted')
-    ax[1].plot(deathsMlpPred, c='g', label='mlp_predicted')
-    ax[1].plot(deathsAdaPred, c='k', label='ada_predicted')
-    ax[1].plot(deathsSvrPred, c='m', label='svr_predicted')
-    ax[1].plot(Y[:, 1], c='b', label='truth')
+    ax[1].plot(deathsRfPred[:final_sample], c='r', label='rf_predicted')
+    ax[1].plot(deathsMlpPred[:final_sample], c='g', label='mlp_predicted')
+    ax[1].plot(deathsAdaPred[:final_sample], c='k', label='ada_predicted')
+    ax[1].plot(deathsSvrPred[:final_sample], c='m', label='svr_predicted')
+    ax[1].plot(deathsTruth[:final_sample], c='b', label='truth')
     ax[1].set_ylabel('num_deaths')
     ax[1].grid('on')
     ax[1].legend()
 
-    ax[2].plot(r0RfPred, c='r', label='rf_predicted')
-    ax[2].plot(r0MlpPred, c='g', label='mlp_predicted')
-    ax[2].plot(r0AdaPred, c='k', label='ada_predicted')
-    ax[2].plot(r0SvrPred, c='m', label='ada_predicted')
-    ax[2].plot(Y[:, 2], c='b', label='truth')
+    ax[2].plot(r0RfPred[:final_sample], c='r', label='rf_predicted')
+    ax[2].plot(r0MlpPred[:final_sample], c='g', label='mlp_predicted')
+    ax[2].plot(r0AdaPred[:final_sample], c='k', label='ada_predicted')
+    ax[2].plot(r0SvrPred[:final_sample], c='m', label='ada_predicted')
+    ax[2].plot(r0Truth[:final_sample], c='b', label='truth')
     ax[2].set_ylabel('R0')
     ax[2].grid('on')
     ax[2].legend()
 
     plt.xticks(range(0, len(dates[1:]), 7), dates[1::7], rotation=90)
+    plt.savefig('images/allpredictions.png')
     plt.show()
 
 
